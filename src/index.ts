31db24991c6d7a03203b0246f42b863c5eb29d22
Nathan Betts <nathan.betts@nhsbt.nhs.uk>
2021-11-19 10:23:21 +0000

    export prop types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-export { default as ActionLink } from './components/action-link';
-export { default as BackLink } from './components/back-link';
-export { default as Button } from './components/button';
-export { default as Card } from './components/card';
-export { default as CardGroup } from './components/card-group';
-export { default as DateInput } from './components/date-input';
-export { default as Details } from './components/details';
-export { default as ErrorMessage } from './components/error-message';
-export { default as ErrorSummary } from './components/error-summary';
-export { default as Expander } from './components/expander';
-export { default as ExpanderGroup } from './components/expander-group';
-export { default as FormGroup } from './components/form-group';
+export { default as ActionLink, ActionLinkProps } from './components/action-link';
+export { default as BackLink, BackLinkProps } from './components/back-link';
+export { default as Button, ButtonProps } from './components/button';
+export { default as Card, CardProps } from './components/card';
+export { default as CardGroup, CardGroupProps } from './components/card-group';
+export { default as DateInput, DateInputProps } from './components/date-input';
+export { default as Details, DetailsProps } from './components/details';
+export { default as ErrorMessage, ErrorMessageProps } from './components/error-message';
+export {
+  default as ErrorSummary,
+  ErrorSummaryProps,
+  ErrorSummaryType,
+  ErrorSummaryLink,
+} from './components/error-summary';
+export { default as Expander, ExpanderProps } from './components/expander';
+export { default as ExpanderGroup, ExpanderGroupProps } from './components/expander-group';
+export { default as FormGroup, FormGroupProps } from './components/form-group';
 export {
   default as Icons,
   ArrowLeft as ArrowLeftIcon,
@@ -26,14 +31,19 @@ export {
   Search as SearchIcon,
   Tick as TickIcon,
 } from './components/icons';
-export { default as Input } from './components/input';
-export { default as InsetText } from './components/inset-text';
-export { default as Link } from './components/link';
-export { default as Pressable } from './components/pressable';
-export { default as Radios } from './components/radios';
-export { default as SectionBreak } from './components/section-break';
-export { default as Select } from './components/select';
-export { default as SummaryList } from './components/summary-list';
-export { default as Tag } from './components/tag';
-export { default as Text } from './components/text';
-export { default as WarningCallout } from './components/warning-callout';
+export { default as Input, InputProps, inputFontStyle } from './components/input';
+export { default as InsetText, InsetTextProps } from './components/inset-text';
+export { default as Link, LinkProps } from './components/link';
+export { default as Pressable, PressableProps } from './components/pressable';
+export { default as Radios, RadiosProps, RadioProps } from './components/radios';
+export { default as SectionBreak, SectionBreakProps } from './components/section-break';
+export { default as Select, SelectProps } from './components/select';
+export {
+  default as SummaryList,
+  SummaryListProps,
+  SummaryViewProps,
+  SummaryTextProps,
+} from './components/summary-list';
+export { default as Tag, TagProps } from './components/tag';
+export { default as Text, TextProps, textStyles } from './components/text';
+export { default as WarningCallout, WarningCalloutProps, WarningCalloutLabelProps } from './components/warning-callout';
